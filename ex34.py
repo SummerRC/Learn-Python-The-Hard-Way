6673127d94da618933277d2a4660860608d5908e
SummerRC <codamaker@gmail.com>
2016-12-21 16:48:10 +0800

    Exercise 31-35

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print animals
+print "The animal at 1 is: %s" % animals[1]
+print "The third(3rd) animal is: %s" % animals[2]
+print "The first(1st) animal is: %s" % animals[0]
+print "The animal at 3 is: %s" % animals[3]
+print "The fifth(5th) animal is: %s" % animals[4]
+print "The animal at 2 is: %s" % animals[2]
+print "The sixth(6th) animal is: %s" % animals[5]
+print "The animal at 4 is: %s" % animals[4]
